bfc51777ba835a06e436aa4c75c67b236bf3941a
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2021-11-07 10:42:09 +0500

    feature test:sqlite db connection

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,17 +2,43 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
 
     public function test_homepage_contains_empty_products_table()
     {
         $response = $this->get('/');
 
-        // $response->assertSee('No products found.');
+        $response->assertSee('No products found.');
+
+        // $response->assertDontSee('No products found.');
 
         $response->assertStatus(200);
+    }
+
+    public function test_homepage_contains_non_empty_products_table()
+    {
+        $product = Product::create([
+            'name' => 'Product 1',
+            'price' => '99.99'
+        ]);
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+
+        $response->assertDontSee('No products found.');
+
+        $response->assertSee('product 1');
+
+        $view_products = $response->viewData('products');
+        dd($view_products);
+
+        $this->assertEquals($product->anem, $view_products->first()->name());
     }
 }
